5eb96c6b7915e3b60a0bbe41e1a96c84a9b56802
ugexe <nlogan@gmail.com>
2012-10-24 21:43:30 -0700

    v0.19r4200

--- lib/Text/Levenshtein/Damerau.pm
+++ lib/Text/Levenshtein/Damerau.pm
@@ -1,12 +1,167 @@
+##############################################################################
+#      $URL: https://github.com/ugexe/Text--Levenshtein--Damerau $
+#     $Date: 2012-10-25 00:02:39 -0500 (Thu, 25 Oct 2012) $
+#   $Author: ugexe $
+# $Revision: 4200 $
+##############################################################################
+
 package Text::Levenshtein::Damerau;
- 
 use utf8;
 use List::Util qw/reduce min/;
+use strict;
+use warnings;
+use Exporter qw( import );
 
-@ISA = qw(Exporter);
-@EXPORT_OK = qw(edistance);
+our @EXPORT_OK = qw( edistance );
+our $VERSION   = '0.19';
 
-our $VERSION = '0.18';
+sub new {
+    my $class = shift;
+    my $self  = {};
+
+    $self->{'source'} = shift;
+
+    bless( $self, $class );
+
+    return $self;
+}
+
+sub dld {
+    my $self = shift;
+    my $args = shift;
+
+    if ( !ref $args ) {
+        return edistance( $self->{'source'}, $args );
+    }
+    elsif ( ref $args->{'list'} eq ref [] ) {
+        my $target_score;
+        foreach my $target ( @{ $args->{'list'} } ) {
+            my $distance = edistance( $self->{'source'}, $target );
+
+            if ( !defined( $args->{max_distance} ) ) {
+                $target_score->{$target} =
+                  edistance( $self->{'source'}, $target );
+            }
+            elsif ( $args->{max_distance} !~ m/^\d+$/xms ) {
+                $target_score->{$target} =
+                  edistance( $self->{'source'}, $target );
+            }
+            elsif ( $distance <= $args->{max_distance} ) {
+                $target_score->{$target} =
+                  edistance( $self->{'source'}, $target );
+            }
+        }
+
+        return $target_score;
+
+    }
+
+}
+
+sub dld_best_match {
+    my $self = shift;
+    my $args = shift;
+
+    if ( defined( $args->{'list'} ) ) {
+        my $hash_ref = $self->dld($args);
+        return reduce { $hash_ref->{$a} < $hash_ref->{$b} ? $a : $b }
+        keys %{$hash_ref};
+    }
+}
+
+sub dld_best_distance {
+    my $self = shift;
+    my $args = shift;
+
+    if ( defined( $args->{'list'} ) ) {
+        my $best_match = $self->dld_best_match( { list => $args->{'list'} } );
+        return $self->dld($best_match);
+    }
+}
+
+sub edistance {
+
+    # Does the actual calculation on a pair of strings
+    my ( $source, $target ) = @_;
+    if ( _null_or_empty($source) ) {
+        if ( _null_or_empty($target) ) {
+            return 0;
+        }
+        else {
+            return length($target);
+        }
+    }
+    elsif ( _null_or_empty($target) ) {
+        return length($source);
+    }
+    elsif ( $source eq $target ) {
+        return 0;
+    }
+
+    my $m   = length($source);
+    my $n   = length($target);
+    my $INF = $m + $n;
+    my %H;
+    $H{0}{0} = $INF;
+
+    for ( 0 ... $m ) {
+        my $i = $_;
+        $H{ $i + 1 }{1} = $i;
+        $H{ $i + 1 }{0} = $INF;
+    }
+    for ( 0 .. $n ) {
+        my $j = $_;
+        $H{1}{ $j + 1 } = $j;
+        $H{0}{ $j + 1 } = $INF;
+    }
+
+    my %sd;
+    for ( 0 .. ( $m + $n ) ) {
+        my $letter = substr( $source . $target, $_ - 1, 1 );
+        $sd{$letter} = 0;
+    }
+
+    for ( 1 .. $m ) {
+        my $i  = $_;
+        my $DB = 0;
+
+        for ( 1 .. $n ) {
+            my $j  = $_;
+            my $i1 = $sd{ substr( $target, $j - 1, 1 ) };
+            my $j1 = $DB;
+
+            if ( substr( $source, $i - 1, 1 ) eq substr( $target, $j - 1, 1 ) )
+            {
+                $H{ $i + 1 }{ $j + 1 } = $H{$i}{$j};
+                $DB = $j;
+            }
+            else {
+                $H{ $i + 1 }{ $j + 1 } =
+                  min( $H{$i}{$j}, $H{ $i + 1 }{$j}, $H{$i}{ $j + 1 } ) + 1;
+            }
+
+            $H{ $i + 1 }{ $j + 1 } = min( $H{ $i + 1 }{ $j + 1 },
+                $H{$i1}{$j1} + ( $i - $i1 - 1 ) + 1 + ( $j - $j1 - 1 ) );
+        }
+
+        $sd{ substr( $source, $i - 1, 1 ) } = $i;
+    }
+
+    return $H{ $m + 1 }{ $n + 1 };
+}
+
+sub _null_or_empty {
+    my $s = shift;
+
+    if ( defined($s) && $s ne {} ) {
+        return 0;
+    }
+
+    return 1;
+}
+
+1;
+__END__
 
 =head1 NAME
 
@@ -49,20 +204,6 @@ Creates and returns a C<Text::Levenshtein::Damerau> object. Takes a scalar with 
 	my $tld = Text::Levenshtein::Damerau->new('Neil');
 	# Creates a new Text::Levenshtein::Damerau object $tld
 
-=cut
-
-
-sub new {
-	my $class = shift;
-	my $self = {};
-
-	$self->{'source'} = shift;
-
-	bless($self, $class);
-
-	return $self;
-}
-
 =head1 METHODS
 
 =head2 $tld->dld
@@ -71,7 +212,7 @@ B<Scalar> Argument: Takes a string to compare with.
 
 Returns: an integer representing the edit distance between the source and the passed argument.
 
-B<Hashref> Argument: Takes a hashref containing
+B<Hashref> Argument: Takes a hashref containing:
 
 =over 4
 
@@ -81,7 +222,7 @@ B<Hashref> Argument: Takes a hashref containing
 
 =back
 
-Returns: hashref with each word from the passed list as keys, and their edit distance (if less than max_distance, which is unlimited by default)
+Returns: hashref with each word from the passed list as keys, and their edit distance (if less than max_distance, which is unlimited by default).
 
 	my $tld = Text::Levenshtein::Damerau->new('Neil');
 	print $tld->dld( 'Niel' ); # prints 1
@@ -94,66 +235,17 @@ Returns: hashref with each word from the passed list as keys, and their edit dis
 	print $d_ref->{'Niel'}; #prints 1
 	print $d_ref->{'Jack'}; #prints 4
 
-	
-=cut
-
-sub dld {
-	my $self = shift;
-	my $args = shift;
-	my $target_score;
-
-	if( ref $args->{'list'} eq 'ARRAY' ) {
-		foreach my $target ( @{$args->{'list'}} ) {
-			my $distance = edistance($source,$target);
-
-			if( !defined($args->{max_distance}) ) {
-				$target_score->{$target} = edistance($self->{'source'},$target);
-			}
-			elsif( $args->{max_distance} !~ m/^\d+$/ ) {
-				$target_score->{$target} = edistance($self->{'source'},$target);
-			}
-			elsif( $distance <= $args->{max_distance} ) {
-				$target_score->{$target} = edistance($self->{'source'},$target);
-			}
-
-			print $target_score->{$target} . ' = ' . $target . "\n";
-		}
-		
-		return $target_score;
-
-	}
-	else {
-		return edistance($self->{'source'},$args);
-	}
-
-	return undef;
-}
-
 =head2 $tld->dld_best_match
 
 Argument: an array reference of strings.
 
 Returns: the string with the smallest edit distance between the source and the array of strings passed.
 
-Takes distance of $tld source against every item in @targets, then returns the string of the best match
+Takes distance of $tld source against every item in @targets, then returns the string of the best match.
 
 	my @name_spellings = ('Niel','Neell','KNiel');
 	print $tld->dld_best_match({ list=> \@name_spellings });
 	# prints Niel
-
-=cut
-
-sub dld_best_match {
-	my $self = shift;
-	my $args = shift;
-
-	if( defined($args->{'list'}) ) {
-		my $hash_ref = $self->dld($args);
-		return reduce { $hash_ref->{$a} < $hash_ref->{$b} ? $a : $b } keys %$hash_ref;
-	}
-
-	return undef;
-}
 
 =head2 $tld->dld_best_distance
 
@@ -166,20 +258,6 @@ Takes distance of $tld source against every item in the passed array, then retur
 	my @name_spellings = ('Niel','Neell','KNiel');
 	print $tld->dld_best_distance({ list => \@name_spellings });
 	# prints 1
-
-=cut
-
-sub dld_best_distance {
-	my $self = shift;
-	my $args = shift;
-
-	if( defined($args->{'list'}) ) {
-		my $best_match = $self->dld_best_match({ list => $args->{'list'} });
-		return $self->dld( $best_match );
-	}
-
-	return undef;
-}
 
 =head1 EXPORTABLE METHODS
 
@@ -195,81 +273,6 @@ Function to take the edit distance between a source and target string. Contains 
 	print edistance('Neil','Niel');
 	# prints 1
 
-=cut
-
-sub edistance {
-	# Does the actual calculation on a pair of strings
-	my($source,$target) = @_;
-	if( _null_or_empty($source) ) {
-		if( _null_or_empty($target) ) {
-			return 0;
-		}
-		else {
-			return length($target);
-		}
-	}
-	elsif( _null_or_empty($target) ) {
-		return length($source);
-	}
-	elsif( $source eq $target ) {
-		return 0;
-	}
-	
-
-	my $m = length($source);
-	my $n = length($target);
-	my $INF = $m + $n;
-	my %H;
-	$H{0}{0} = $INF;
-
-	for(my $i = 0; $i <= $m; $i++) { $H{$i + 1}{1} = $i; $H{$i + 1}{0} = $INF; }
-	for(my $j = 0; $j <= $n; $j++) { $H{1}{$j + 1} = $j; $H{0}{$j + 1} = $INF; }
-
-	my %sd;
-	for(my $key = 0; $key < ($m + $n); $key++) {
-		my $letter = substr($source . $target, $key-1, 1);
-		$sd{$letter} = 0;
-	}
-	
-
-	for(my $i = 1; $i <= $m; $i++) {
-		my $DB = 0;
-
-		for(my $j = 1; $j <= $n; $j++) {
-			my $i1 = $sd{substr($target, $j-1, 1)};
-			my $j1 = $DB;
-
-			if( substr($source, $i-1, 1) eq substr($target, $j-1, 1) ) {
-				$H{$i + 1}{$j + 1} = $H{$i}{$j};
-				$DB = $j;
-			}
-			else {
-				$H{$i + 1}{$j + 1} = min($H{$i}{$j}, $H{$i + 1}{$j}, $H{$i}{$j + 1}) + 1;
-			}
-
-			$H{$i + 1}{$j + 1} = min($H{$i + 1}{$j + 1}, $H{$i1}{$j1} + ($i - $i1 - 1) + 1 + ($j - $j1 - 1));
-		}
-
-		$sd{substr($source, $i-1, 1)} = $i;
-	}
-
-	return $H{$m + 1}{$n + 1};
-}
-
-sub _null_or_empty {
-	my $s = shift;
-	
-	if( defined($s) && $s ne '') {
-		return 0;
-	}
-	
-	return 1;
-}
-
-1;
-__END__
-
-
 =head1 SEE ALSO
 
 =over 4
@@ -280,11 +283,17 @@ __END__
 
 =back
 
+=head1 BUGS
+
+Please report bugs to:
+
+L<https://rt.cpan.org/Public/Dist/Display.html?Name=Text-Levenshtein-Damerau>
+
 =head1 AUTHOR
 
 ugexe <F<ug@skunkds.com>>
 
-=head1 LICENSE
+=head1 LICENSE AND COPYRIGHT
 
 This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
 
---
+++ t/02_edistance.t
@@ -0,0 +1,25 @@
+use strict;
+use warnings;
+
+use Test::More tests => 14;
+use Text::Levenshtein::Damerau qw/edistance/;;
+
+is( edistance('four','four'), 0, 'test edistance matching');
+is( edistance('four','for'), 1, 'test edistance insertion');
+is( edistance('four','fourth'), 2, 'test edistance deletion');
+is( edistance('four','fuor'), 1, 'test edistance transposition');
+is( edistance('four','fxxr'), 2, 'test edistance substitution');
+is( edistance('four','FOuR'), 3, 'test edistance case');
+is( edistance('four',''), 4, 'test edistance target empty');
+is( edistance('','four'), 4, 'test edistance source empty');
+is( edistance('',''), 0, 'test edistance source & target empty');
+
+# Test some utf8
+use utf8;
+binmode STDOUT, ":encoding(utf8)";
+is( edistance('ⓕⓞⓤⓡ','ⓕⓞⓤⓡ'), 0, 'test edistance matching (utf8)');
+is( edistance('ⓕⓞⓤⓡ','ⓕⓞⓡ'), 1, 'test edistance insertion (utf8)');
+is( edistance('ⓕⓞⓤⓡ','ⓕⓞⓤⓡⓣⓗ'), 2, 'test edistance deletion (utf8)');
+is( edistance('ⓕⓞⓤⓡ','ⓕⓤⓞⓡ'), 1, 'test edistance transposition (utf8)');
+is( edistance('ⓕⓞⓤⓡ','ⓕⓧⓧⓡ'), 2, 'test edistance substitution (utf8)');
+
